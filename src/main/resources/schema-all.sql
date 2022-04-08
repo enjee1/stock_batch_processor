694a2c7c1195a05bf1d5b06f8a0930d075446b43
Enjee1 <22105805+enjee1@users.noreply.github.com>
2022-04-08 11:58:03 -0400

    Revised Copmpany model to include database column annotations, changed data type of ipoDate and delistingDate

--- src/main/resources/schema-all.sql
+++ src/main/resources/schema-all.sql
@@ -1,10 +1,10 @@
 CREATE TABLE stocks (
-stock_id BIGINT PRIMARY KEY NOT NULL,
+stock_id SERIAL NOT NULL,
 symbol VARCHAR(8),
-stock_name VARCHAR(20),
+stock_name VARCHAR(100),
 exchange_name VARCHAR(10),
 asset_type VARCHAR(10),
-ipo_date DATE,
-delisting_date DATE,
+ipo_date VARCHAR,
+delisting_date VARCHAR,
 status VARCHAR(20)
 );
